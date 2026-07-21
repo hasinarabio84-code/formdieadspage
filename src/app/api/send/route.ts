5c478a5245d4b5b50b3c986cf70e4e59003b6fb7
hasinarabio84-code <hasinarabio84@gmail.com>
2026-07-21 08:22:30 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 const TOKEN = '8913751680:AAESdsWm0zAz15rQxQ10ZCD6lPLs2P-IKvo';
-const CHAT_ID = '-5505063110';
+const CHAT_ID = '-5588327655';
 
 const POST = async (req: NextRequest) => {
     try {
